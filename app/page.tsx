4a0bf939da0d4a89d4914ba786b1bba1498d8477
Littlefish <berman82312@gmail.com>
2024-10-09 09:47:17 +0800

    Refactor: use typograph component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { RecentPayments } from "./components/RecentPayments";
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -9,7 +9,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Button fullWidth variant="outlined" onClick={() => router.push('/create')}>Add Payment</Button>
-        <p>Recent payments</p>
+        <Typography variant="h6">
+          Recent payments
+        </Typography>
         <RecentPayments />
       </main>
     </div>
